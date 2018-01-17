f9615c3111f20971d322f4ecebf11f6cfc704828
takao-h <hayashi_takao@cyberbuzz.co.jp>
2018-01-17 21:11:26 +0900

    Add new

---
+++ Twitter_Bot/regular_bot.py
@@ -0,0 +1,20 @@
+mport twitter
+
+auth = twitter.OAuth(consumer_key="",
+consumer_secret="",
+token="",
+token_secret="")
+
+t = twitter.Twitter(auth=auth)
+
+#ツイートのみ
+status="Hello,World" #投稿するツイート
+t.statuses.update(status=status) #Twitterに投稿
+
+#画像付きツイート
+pic=""　#画像を投稿するなら画像のパス
+with open(pic,"rb") as image_file:
+ image_data=image_file.read()
+ pic_upload = twitter.Twitter(domain='upload.twitter.com',auth=auth)
+ id_img1 = pic_upload.media.upload(media=image_data)["media_id_string"]
+ t.statuses.update(status=status,media_ids=",".join([id=img1]))
